e1cfda8aa685e2889238bc3978e7a066cc36ea01
dongguk kim <kdd0419@naver.com>
2019-06-05 11:38:49 +0900

    progress bar 구성

--- fe/src/App.vue
+++ fe/src/App.vue
@@ -1,37 +1,56 @@
 <template>
   <v-app>
     <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+      <v-toolbar-title class="headline">
+        <span>집에 가고 싶어</span>
       </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
     </v-toolbar>
-
     <v-content>
-      <HelloWorld/>
+    `<v-flex xs12 sm6 offset-sm3>
+      <v-card>
+        <v-list two-line>
+          <v-list-tile
+            value="true"
+            v-for="(item, i) in value"
+            :key="i"
+            :to="item.to"
+          >
+            <v-list-tile-content>
+              <v-list-tile-title>{{item.title}}</v-list-tile-title>
+              <v-list-tile-sub-title>
+                <v-progress-linear
+                  v-model="item.value"
+                  height="20"
+                  color="info"
+                >
+                  <span class="black--text">{{item.value}}%</span>
+                </v-progress-linear>
+              </v-list-tile-sub-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+      </v-card>
+    </v-flex>`
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  },
   data () {
     return {
-      //
+      value: [
+        {
+          title: '졸업일',
+          value: 70
+        },
+        {
+          title: '집으로 돌아간다!',
+          value: 80
+        }
+      ]
     }
   }
 }
