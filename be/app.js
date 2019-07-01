d71765ef03d4ea0e5872a1390bb4fcffd5d2582e
kdd0419 <kdd0419@naver.com>
2019-07-01 15:01:35 +0900

    install cors

--- be/app.js
+++ be/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -21,6 +22,7 @@ app.use(cookieParser());
 app.use('/api', require('./routes/api'));
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors())
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
