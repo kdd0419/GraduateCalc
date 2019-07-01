8bebb3ea9dfaa301bd42a9a3114c8be3bd3cf3da
Junwon <2junewon@naver.com>
2019-07-01 13:46:42 +0900

    도움말

--- fe/src/App.vue
+++ fe/src/App.vue
@@ -8,6 +8,34 @@
     <v-content :style="{'background-image': 'url(' + require('./back.png') + ')', 'background-size': '100% 100%'}">
       <router-view></router-view>
     </v-content>
+    <v-stepper non-linear>
+      <v-stepper-header>
+        <v-stepper-step
+          editable
+          step="1"
+        >
+          동국이를 때리세요
+        </v-stepper-step>
+
+        <v-divider></v-divider>
+
+        <v-stepper-step
+          editable
+          step="2"
+        >
+          Create an ad group
+        </v-stepper-step>
+
+        <v-divider></v-divider>
+
+        <v-stepper-step
+          step="3"
+          editable
+        >
+          Create an ad
+        </v-stepper-step>
+      </v-stepper-header>
+    </v-stepper>
   </v-app>
 </template>
 
